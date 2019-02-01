3409223c44963a64f1d329203eaed07620b0f945
Micaela Jawor <micaelajawor@yahoo.com>
2019-02-01 08:46:10 -0800

    create price calculator prototype

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,13 @@ var pizzaToppingsPrice = {
   "pineapple": .50,
 }
 
+Pizza.prototype.calculatePrice = function() {
+  var totalPizzaToppingPrice = 0;
+  for (var index = 0; index < this.pizzaToppings.length; index++) {
+    totalPizzaToppingPrice += pizzaToppingsPrice(this.pizzaToppings[index]);
+  }
+  this.price = (pizzaSizePrice(this.size) + totalPizzaToppingPrice);
+}
 
 // User Interface
 $(document).ready(function() {
